a3dac82f7d796a1e41c5dac68aaf777ab488a3ca
David McGaughey <mcgaughey@gmail.com>
2016-12-15 16:50:30 -0500

    barebones ggplot for 1000g and sample tsne

--- scripts/plot.R
+++ scripts/plot.R
@@ -6,4 +6,16 @@ import(tidyverse)
 
 args <- commandArgs(trailingOnly=True)
 plink_raw <- data.frame(fread(args[1]))
+ped <- data.frame(fread('~/git/ethnicity_imputation/data/1000g_20130606.ped'))
 
+geno_mat <- as.matrix(plink_raw[,7:ncol(plink_raw)])
+
+tsne <- Rtsne(geno_mat, perplexity=10, theta=0.0, check_duplicates=FALSE)
+
+plot <- data.frame(tsne$Y)
+
+plot$ID <- plink_raw$IID
+
+plot <- left_join(plot, ped, by=c('ID'='Individual.ID'))
+
+ggplot(all,aes(x=X1,y=X2,colour=Population)) + geom_point() + scale_colour_brewer(palette='Set1') + theme_bw()
