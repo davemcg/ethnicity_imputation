65190a9b5bcd7aacfe8b3e4cf172e0a64a55ad79
David McGaughey <davemcg@users.noreply.github.com>
2016-12-16 15:42:33 -0500

    library not import

--- scripts/plot.R
+++ scripts/plot.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 
-import(data.table)
-import(Rtsne)
-import(tidyverse)
+library(data.table)
+library(Rtsne)
+library(tidyverse)
 
 args <- commandArgs(trailingOnly=True)
 plink_raw <- data.frame(fread(args[1]))
