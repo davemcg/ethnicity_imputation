c019337dd2b8da5fc1cd9bedb7e9ed47b2e52b18
David McGaughey <davemcg@users.noreply.github.com>
2016-12-16 17:20:59 -0500

    takes highest var positions

--- scripts/plot.R
+++ scripts/plot.R
@@ -9,13 +9,14 @@ plink_raw <- data.frame(fread(args[1]))
 ped <- data.frame(fread('~/git/ethnicity_imputation/data/1000g_20130606.ped'))
 
 geno_mat <- as.matrix(plink_raw[,7:ncol(plink_raw)])
+geno_mat_clean <- geno_mat[ , colSums(is.na(geno_mat)) == 0]
+# Take highest variance
+var <- apply(geno_mat_clean,2,var)
+geno_var <- geno_mat_clean[,var>0.5]
 
-tsne <- Rtsne(geno_mat, perplexity=10, theta=0.0, check_duplicates=FALSE)
+tsne <- Rtsne(geno_var, perplexity=50, theta=0.0, check_duplicates=FALSE)
 
 plot <- data.frame(tsne$Y)
-
 plot$ID <- plink_raw$IID
-
 plot <- left_join(plot, ped, by=c('ID'='Individual.ID'))
-
 ggplot(all,aes(x=X1,y=X2,colour=Population)) + geom_point() + scale_colour_brewer(palette='Set1') + theme_bw()
