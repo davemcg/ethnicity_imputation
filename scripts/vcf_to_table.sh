17b44a2a8f220ba2fdf7cbc4a334bc6570850815
David McGaughey <mcgaughey@gmail.com>
2017-06-02 14:02:40 -0400

    vcf to tabular format

--- scripts/vcf_to_table.sh
+++ scripts/vcf_to_table.sh
@@ -6,4 +6,4 @@ merged_vcf=$1
 out_prefix=$2
 
 # puts the vcf in a tabular format for R
-plink --vcf $merged-vcf --vcf-filter --vcf-require-gt --double-id --recode A --out $out_prefix
+plink --vcf $merged_vcf --vcf-filter --vcf-require-gt --double-id --recode A --out $out_prefix
