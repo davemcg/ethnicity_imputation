fc73c0ca9d0542938f177d8a058037b1c5375dab
David McGaughey <mcgaughey@gmail.com>
2017-06-05 13:02:08 -0400

    transposed output, remove NA rows

--- scripts/vcf_to_table.sh
+++ scripts/vcf_to_table.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-module load plink/1.9
+module load  plink/1.9.0-beta4.1
 
 merged_vcf=$1
 out_prefix=$2
 
 # puts the vcf in a tabular format for R
-plink --vcf $merged_vcf --vcf-filter --vcf-require-gt --double-id --recode A --out $out_prefix
+plink --vcf $merged_vcf --vcf-filter --vcf-require-gt --double-id --recode A-transpose --vcf-half-call m --out $out_prefix --allow-extra-chr
+
+# remove rows with NA
+grep -v NA $out_prefix.traw > $out_prefix.noNA.traw
